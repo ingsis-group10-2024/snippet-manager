7c880730b2a9822f5b49bdd26bb7a4d845be887a
ccatalini <camila.catalini@ing.austral.edu.ar>
2024-05-26 17:31:29 -0300

    auth0 integrated

--- src/main/kotlin/ingsis/snippetmanager/controller/SnippetController.kt
+++ src/main/kotlin/ingsis/snippetmanager/controller/SnippetController.kt
@@ -5,7 +5,6 @@ import ingsis.snippetmanager.dto.CreateSnippetDTO
 import ingsis.snippetmanager.service.SnippetService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
