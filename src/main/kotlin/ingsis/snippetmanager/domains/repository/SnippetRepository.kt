4b73902529b72a8909fce8ed53d16e4171f5efcd
ccatalini <camila.catalini@ing.austral.edu.ar>
2024-05-27 19:36:52 -0300

    permission pre-commit hooks

--- src/main/kotlin/ingsis/snippetmanager/domains/repository/SnippetRepository.kt
+++ src/main/kotlin/ingsis/snippetmanager/domains/repository/SnippetRepository.kt
@@ -2,10 +2,8 @@ package ingsis.snippetmanager.domains.repository
 
 import ingsis.snippetmanager.domains.model.Snippet
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 import java.util.*
 
-
-interface SnippetRepository : JpaRepository<Snippet, UUID>{
+interface SnippetRepository : JpaRepository<Snippet, UUID> {
     fun findByUsername(username: String): List<Snippet>
 }
