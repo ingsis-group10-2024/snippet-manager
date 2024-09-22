c321fb98bf928ed4dac2c37604604b3de6f771af
ccatalini <camila.catalini@ing.austral.edu.ar>
2024-09-22 19:30:02 -0300

    first commit

---
+++ src/main/kotlin/ingisis/manager/SnippetManagerApplication.kt
@@ -0,0 +1,11 @@
+package ingisis.manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SnippetManagerApplication>(*args)
+}
