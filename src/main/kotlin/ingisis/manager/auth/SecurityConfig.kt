85959c6d60ecab07970a9a244dd50d325858175c
maxim <maximiliano.maglio@ing.austral.edu.ar>
2024-11-02 19:06:57 -0300

    mmaxi(fix) - SecurityConfig

--- src/main/kotlin/ingisis/manager/auth/SecurityConfig.kt
+++ src/main/kotlin/ingisis/manager/auth/SecurityConfig.kt
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
 import org.springframework.security.web.SecurityFilterChain
 
 /**
@@ -20,13 +22,22 @@ class SecurityConfig {
             .authorizeHttpRequests { authorize ->
                 authorize
                     .anyRequest()
-                    .authenticated() // Cualquier otra petición debe estar autenticada
+                    .authenticated()
             }.cors(withDefaults())
             .oauth2ResourceServer { oauth2 ->
-                oauth2
-                    .jwt(withDefaults())
+                oauth2.jwt { jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()) }
             }.build()
 
     @Bean
     fun jwtDecoder(): JwtDecoder = NimbusJwtDecoder.withJwkSetUri("https://dev-8f0uq116yhuzay1x.us.auth0.com/.well-known/jwks.json").build()
+
+    @Bean
+    fun jwtAuthenticationConverter(): JwtAuthenticationConverter {
+        val converter = JwtAuthenticationConverter()
+        converter.setJwtGrantedAuthoritiesConverter { jwt ->
+            val scopes = jwt.claims["scope"]?.toString()?.split(" ") ?: emptyList()
+            scopes.map { SimpleGrantedAuthority(it) }
+        }
+        return converter
+    }
 }
