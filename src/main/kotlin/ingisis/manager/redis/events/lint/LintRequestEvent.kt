6157e65351b31db5f2845563aab1ffc8e2b8734e
ccatalini <camila.catalini@ing.austral.edu.ar>
2024-11-14 12:24:21 -0300

    refactor[snippet-entity]: compilation status attribute added

--- src/main/kotlin/ingisis/manager/redis/events/lint/LintRequestEvent.kt
+++ src/main/kotlin/ingisis/manager/redis/events/lint/LintRequestEvent.kt
@@ -3,7 +3,7 @@ package ingisis.manager.redis.events.lint
 import ingisis.manager.redis.events.rules.LinterRules
 import java.util.UUID
 
-data class LintRequest(
+data class LintRequestEvent(
     val snippetId: UUID,
     val snippetContent: String,
     val rules: LinterRules,
--- src/main/kotlin/ingisis/manager/snippet/persistance/entity/Snippet.kt
+++ src/main/kotlin/ingisis/manager/snippet/persistance/entity/Snippet.kt
@@ -1,5 +1,6 @@
 package ingisis.manager.snippet.persistance.entity
 
+import ingisis.manager.snippet.model.enums.CompilationState
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
@@ -14,17 +15,20 @@ import java.util.UUID
 data class Snippet(
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
     val id: String = UUID.randomUUID().toString(),
-    @Column(nullable = false)
+    @Column(name = "name", nullable = false)
+    var name: String,
+    @Column(name = "content", nullable = false)
+    var content: String, // debería pasar a ser un id para guardar el contenido en un bucket
+    @Column(name = "author-id", nullable = false)
     val authorId: String,
+    @Column(name = "compilation-state", nullable = false)
+    var compilationState: CompilationState = CompilationState.PENDING,
     @CreationTimestamp
     val createdAt: LocalDateTime = LocalDateTime.now(),
     @UpdateTimestamp
     var updatedAt: LocalDateTime = LocalDateTime.now(),
-    @Column(nullable = false)
-    var name: String,
-    @Column(nullable = false)
-    var content: String,
     @Column(nullable = false)
     var language: String,
     @Column(nullable = false)
