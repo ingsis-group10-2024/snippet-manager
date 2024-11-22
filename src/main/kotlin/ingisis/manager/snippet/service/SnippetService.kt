b86d72a3dd00bb73704e95a376eb63d70e4d634d
maxim <maximiliano.maglio@ing.austral.edu.ar>
2024-11-22 00:16:32 -0300

    features(new relic): added new relic for logging and monitoring

--- src/main/kotlin/ingisis/manager/snippet/service/SnippetService.kt
+++ src/main/kotlin/ingisis/manager/snippet/service/SnippetService.kt
@@ -12,6 +12,8 @@ import ingisis.manager.snippet.model.dto.rest.permission.SnippetDescriptor
 import ingisis.manager.snippet.model.dto.rest.runner.ValidationResponse
 import ingisis.manager.snippet.persistance.entity.Snippet
 import ingisis.manager.snippet.persistance.repository.SnippetRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpEntity
@@ -31,10 +33,16 @@ class SnippetService
         private val restTemplate: RestTemplate,
         private val azuriteService: AzuriteService,
     ) {
-        fun getSnippetById(id: String): Snippet =
-            repository.findById(id).orElseThrow {
+        private val logger: Logger = LoggerFactory.getLogger(SnippetService::class.java)
+
+        fun getSnippetById(id: String): Snippet {
+            logger.info("Searching snippet with ID: $id")
+
+            return repository.findById(id).orElseThrow {
+                logger.error("Snippet with ID $id not found")
                 SnippetNotFoundException("Snippet with ID $id not found")
             }
+        }
 
         fun getSnippetsByUserId(userId: String): List<Snippet> = repository.findByAuthorId(userId)
 
@@ -43,38 +51,59 @@ class SnippetService
             principal: Principal,
             authorizationHeader: String,
         ): Snippet {
-            val authorId = principal.name
-            val snippet =
-                Snippet(
-                    name = input.name,
-                    content = "", // Initially empty
-                    language = input.language,
-                    languageVersion = input.languageVersion,
-                    authorId = authorId,
-                    extension = input.extension,
+            try {
+                logger.info("Creating a new snippet for user: ${principal.name}")
+                val authorId = principal.name
+                val snippet =
+                    Snippet(
+                        name = input.name,
+                        content = "", // Initially empty
+                        language = input.language,
+                        languageVersion = input.languageVersion,
+                        authorId = authorId,
+                        extension = input.extension,
+                    )
+
+                logger.info("Blob URL for snippet: $snippet is: ${snippet.content}")
+
+                val blobUrl = azuriteService.uploadContentToAzurite(snippet.id, input.content)
+                snippet.content = blobUrl
+
+                println("Creating snippet: $snippet")
+
+                val lintResult =
+                    validateSnippet(
+                        snippet.name,
+                        input.content,
+                        snippet.language,
+                        snippet.languageVersion,
+                        authorizationHeader,
+                    )
+
+                logger.info("Lint results for snippet: $snippet : $lintResult")
+
+                // Throws exceptions if the snippet is invalid
+                if (!lintResult.isValid) {
+                    val errors = lintResult.errors ?: emptyList() // Get the errors from the response
+                    logger.error("Errors validating snippet: $errors")
+                    throw InvalidSnippetException(errors)
+                }
+
+                logger.info("Saving snippet to database...")
+                val savedSnippet = repository.save(snippet)
+                logger.info("Snippet saved: $savedSnippet")
+
+                giveOwnerPermissionToSnippet(
+                    authorId = savedSnippet.authorId,
+                    snippetId = savedSnippet.id,
+                    authorizationHeader = authorizationHeader,
                 )
-
-            val blobUrl = azuriteService.uploadContentToAzurite(snippet.id, input.content)
-            snippet.content = blobUrl
-
-            println("Creating snippet: $snippet")
-
-            val lintResult =
-                validateSnippet(snippet.name, input.content, snippet.language, snippet.languageVersion, authorizationHeader)
-
-            // Throws exceptions if the snippet is invalid
-            if (!lintResult.isValid) {
-                val errors = lintResult.errors ?: emptyList() // Get the errors from the response
-                throw InvalidSnippetException(errors)
+                logger.info("Owner permission given to snippet: $savedSnippet")
+                return savedSnippet
+            } catch (e: Exception) {
+                logger.error("Error creating snippet: ${e.message}")
+                throw e
             }
-
-            val savedSnippet = repository.save(snippet)
-            giveOwnerPermissionToSnippet(
-                authorId = savedSnippet.authorId,
-                snippetId = savedSnippet.id,
-                authorizationHeader = authorizationHeader,
-            )
-            return savedSnippet
         }
 
         private fun giveOwnerPermissionToSnippet(
@@ -82,6 +111,9 @@ class SnippetService
             snippetId: String,
             authorizationHeader: String,
         ) {
+            logger.info("Giving OWNER permission to snippet with ID: $snippetId")
+            logger.info("Calling permission service to give permission...")
+
             val url = "http://snippet-permission:8080/permission"
 
             val request =
@@ -104,7 +136,10 @@ class SnippetService
             userId: String,
             authorizationHeader: String,
         ): Snippet {
+            logger.info("Updating snippet with ID: $id")
             val snippet = getSnippetById(id)
+
+            logger.info("Retrieved snippet: $snippet")
 
             // Check if the user has permission to update the snippet
             val hasPermission =
@@ -115,8 +150,13 @@ class SnippetService
                     requiredPermission = "OWNER",
                 )
             if (!hasPermission) {
+                logger.error("User does not have permission to update snippet: $snippet")
                 throw SecurityException("User does not have permission to update this snippet.")
             }
+
+            logger.info("Found OWNER permission for snippet: $snippet. UserId is: $userId")
+
+            logger.info("Validating updated content...")
 
             // Validate the updated content
             val lintResult =
@@ -128,12 +168,22 @@ class SnippetService
                     authorizationHeader = authorizationHeader,
                 )
             if (!lintResult.isValid) {
+                logger.error("Updated content is invalid: $lintResult")
                 throw InvalidSnippetException(lintResult.errors ?: emptyList())
             }
 
+            logger.info("Updated content is valid: $lintResult")
+
+            logger.info("Uploading updated content to azurite...")
+
             // Update azurite content
             val updatedContentUrl = azuriteService.uploadContentToAzurite(snippet.id, input.content)
+            logger.info("Updated content uploaded to azurite: $updatedContentUrl")
+
             val updatedSnippet = snippet.copy(content = updatedContentUrl)
+
+            logger.info("Snippet updated: $updatedSnippet. Saving to database...")
+
             return repository.save(updatedSnippet)
         }
 
@@ -142,6 +192,9 @@ class SnippetService
             principal: Principal,
             authorizationHeader: String,
         ) {
+            logger.info("Deleting snippet with ID: $id")
+
+            logger.info("Retrieving snippet...")
             val snippet = getSnippetById(id)
 
             val hasPermission =
@@ -152,10 +205,20 @@ class SnippetService
                     requiredPermission = "OWNER",
                 )
             if (!hasPermission) {
+                logger.error("User does not have permission to delete snippet: $snippet")
                 throw SecurityException("User does not have permission to delete this snippet.")
             }
 
+            logger.info("User has OWNER permission to delete snippet: $snippet")
+
+            logger.info("Deleting snippet content from azurite...")
+
             azuriteService.deleteContentFromAzurite(snippet.id)
+
+            logger.info("Content deleted from azurite: $snippet")
+
+            logger.info("Deleting snippet from database...")
+
             repository.deleteById(id)
         }
 
@@ -166,8 +229,12 @@ class SnippetService
             languageVersion: String,
             authorizationHeader: String,
         ): ValidationResponse {
+            logger.info("Validating snippet: $name")
+
             val snippetRequest =
                 SnippetRequest(name = name, content = content, languageVersion = languageVersion, language = language)
+
+            logger.info("Sending snippet to runner service for validation...")
 
             val headers: MultiValueMap<String, String> = LinkedMultiValueMap()
             headers.add("Authorization", authorizationHeader)
@@ -180,7 +247,13 @@ class SnippetService
                     requestEntity,
                     ValidationResponse::class.java,
                 )
-            return response.body ?: throw RuntimeException("Error obtaining response from runner service")
+            return if (response.body != null) {
+                logger.info("Snippet validated: ${response.body}")
+                response.body!!
+            } else {
+                logger.error("Error validating snippet: ${response.statusCode}")
+                throw Exception("Error validating snippet: ${response.statusCode}")
+            }
         }
 
         private fun checkUserPermission(
@@ -189,6 +262,8 @@ class SnippetService
             authorizationHeader: String,
             requiredPermission: String,
         ): Boolean {
+            logger.info("Checking if user has permissions by calling permission service...")
+
             val headers: MultiValueMap<String, String> = LinkedMultiValueMap()
             headers.add("Authorization", authorizationHeader)
             headers.add("Content-Type", "application/json")
@@ -205,10 +280,17 @@ class SnippetService
                 )
 
             if (response.statusCode.is2xxSuccessful) {
-                val permissions = response.body as? List<String> ?: emptyList()
+                val permissions =
+                    if (response.body is List<*>) {
+                        (response.body as List<*>).filterIsInstance<String>()
+                    } else {
+                        emptyList()
+                    }
+                logger.info("Permissions found: $permissions")
                 return permissions.contains(requiredPermission)
             } else {
-                println("Error: ${response.statusCode} - ${response.statusCodeValue}")
+                println("Error: ${response.statusCode} - ${response.statusCode.value()}")
+                logger.error("Error checking permissions: ${response.statusCode}")
                 return false
             }
         }
@@ -219,6 +301,8 @@ class SnippetService
             principal: Principal,
             authorizationHeader: String,
         ): Snippet {
+            logger.info("Processing file and creating snippet...")
+
             val content = file.inputStream.bufferedReader().use { it.readText() }
 
             val snippetData = input.copy(content = content)
@@ -237,8 +321,10 @@ class SnippetService
             snippetId: String,
             authorizationHeader: String,
         ): SnippetDescriptor {
+            logger.info("Getting snippet details for snippet with ID: $snippetId")
+
             val snippet = getSnippetById(snippetId)
-            println("Snippet found: $snippet")
+            logger.info("Snippet found: $snippet")
 
             val snippetContent = getSnippetContent(snippet.content)
             println("Snippet content: $snippetContent")
@@ -272,8 +358,13 @@ class SnippetService
             pageSize: Int,
             authorizationHeader: String,
         ): PaginatedSnippetResponse {
+            logger.info("Getting snippets' details for user: ${principal.name}")
             val pageable = PageRequest.of(page, pageSize)
             val userSnippetsPage = repository.findByAuthorId(principal.name, pageable)
+
+            logger.info("${principal.name}'s snippets: $userSnippetsPage")
+
+            logger.info("Getting shared snippets...")
 
             // Filter snippets that the user has permission to read
             val sharedSnippetsPage =
@@ -288,8 +379,10 @@ class SnippetService
                         )
                     }
 
+            logger.info("Shared snippets: $sharedSnippetsPage")
+
             val combinedSnippets = userSnippetsPage.content + sharedSnippetsPage
-            println("One's own snippets found: ${combinedSnippets[0]}") // DEBUG
+            logger.info("Combined snippets: $combinedSnippets")
 
             // Convert the page to a list of SnippetDescriptor
             val snippets =
